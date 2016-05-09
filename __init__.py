2fd63962e8a1fb18372eb845cee2b5709a1fc7da
Shane Brown <contact@shane-brown.ca>
2016-05-09 11:08:35 -0400

    Added argparse

--- __init__.py
+++ __init__.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python
 
+import argparse
 from Alarm import Alarm
+
+def parseArguments():
+    parser = argparse.ArgumentParser(description="Simple alarm app for timespans and dates")
+    parser.add_argument("action", help="Action you wish to perform")
+    parser.add_argument("time", help="time Ex: 10h10m03s", required=False)
+    parser.add_argument("-d", "--daemon", help="Start alarm daemon")
+    args = parser.parse_args()
+    return args
+
+args = parseArguments()
 
 test = Alarm("now", "a new alarm")
 
