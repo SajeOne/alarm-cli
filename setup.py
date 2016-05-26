f99a5c61f9e077b6712ee5f3d1d833b315eba643
Shane Brown <contact@shane-brown.ca>
2016-05-25 21:15:34 -0400

    fixed setup and deleted junk

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from setuptools import setup
 
 setup(name='alarm-cli',
         version='0.1',
-        description='Simple alarm app for the command line'
-        url='https://github.com/SajeOne/alarm-cli'
-        author='Shane "SajeOne" Brown'
-        author_email='contact@shane-brown.ca'
+        description='Simple alarm app for the command line',
+        url='https://github.com/SajeOne/alarm-cli',
+        author='Shane "SajeOne" Brown',
+        author_email='contact@shane-brown.ca',
         license='GPL3',
         packages['alarm-cli'],
         zip_safe=False)
