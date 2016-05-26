a186dca4716f807ac314a993f9d0298a2ec651fb
Shane Brown <contact@shane-brown.ca>
2016-05-25 22:56:02 -0400

    removed packages from setup

--- setup.py
+++ setup.py
@@ -9,5 +9,4 @@ setup(name='alarm-cli',
         author='Shane "SajeOne" Brown',
         author_email='contact@shane-brown.ca',
         license='GPL3',
-        packages=['alarm-cli'],
         zip_safe=False)
